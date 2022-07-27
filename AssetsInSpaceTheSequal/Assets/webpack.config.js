988879254eb54c41d99c2a123d09fd38726261cb
Mario van Zeist <m.vanzeist@home.nl>
2022-07-27 10:08:57 +0200

    Added second project which has the Build tasks integrated into the csproj file for easier testing.

---
+++ AssetsInSpaceTheSequal/Assets/webpack.config.js
@@ -0,0 +1,14 @@
+﻿
+module.exports = (env, args) => ({
+    resolve: { extensions: ['.scss'] },
+    devtool: args.mode == 'development' ? 'source-map' : false,
+
+    entry: {
+        'components.bwa': './css/components.bwa.scss'
+    },
+    module: {
+        rules: [
+            { test: /\.(s(a|c)ss)$/, use: ['css-loader', 'sass-loader'] }
+        ],
+    }
+})
